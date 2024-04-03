4c5d162376425533497229bc1ef6008a9d9dcf11
YongSM <sukmin1116@gmail.com>
2024-04-03 17:17:06 +0900

    템플릿 및 경로 반영

---
+++ DBmanager.py
@@ -0,0 +1,18 @@
+class DBmanager:
+    def __init__(self, config, cursor):
+        self.config = config
+        self,cursor = cursor
+        
+        
+    
+    def insert():
+        return 
+    
+    def updatetDatabase():
+        return 
+    
+    def insertDatabase():
+        return 
+    
+    def insertDatabase():
+        return 
